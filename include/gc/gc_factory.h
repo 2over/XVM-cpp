d06080c289144fce03b46247c9272948961b6a7b
xiehao <mysunshine2126@gmail.com>
2024-12-02 02:48:15 -0800

    feat: memory_cell.h memory_chunk.h

---
+++ include/gc/gc_factory.h
@@ -0,0 +1,6 @@
+//
+// Created by xiehao on 2024/12/2.
+//
+
+#include "../common.h"
+#include "../memory/memory_pool.h"
---
+++ include/gc/interface/garbage_collect.h
@@ -0,0 +1,8 @@
+#pragma once
+
+using namespace std;
+
+class GarbageCollect {
+public:
+    virtual void run() = 0;
+};
